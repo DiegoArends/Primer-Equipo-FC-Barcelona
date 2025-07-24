74d3a912930581df65174c1dcf7b4c74db5a1106
Diego <diegoarends17@gmail.com>
2025-07-24 19:29:33 -0400

    deply4

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import fondoVidrio from './assets/fondo-con-textura-de-vidrio-patron.jpg';
+import campNou from './assets/El_Camp_Nou_en_un_partido_de_la_Uefa_Champions_League.webp';
 import './App.css';
 
 function App() {
@@ -14,16 +16,39 @@ function App() {
   };
 
   useEffect(() => {
-    //axios.get('http://localhost:3001/players')
     axios.get(`${process.env.REACT_APP_API_URL}/players`)
       .then(res => setPlayers(res.data))
       .catch(err => console.error(err));
   }, []);
 
   return (
-    <div>
+    <div
+      style={{
+        backgroundColor: '#af0d33',
+        backgroundImage: `radial-gradient(circle , rgba(56, 4, 4, 0.7) 0%, rgba(30, 131, 253, 0.863) 100%), url(${fondoVidrio})`,
+        backgroundBlendMode: 'multiply',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        fontFamily: "'Segoe UI', Arial, sans-serif",
+        margin: 0,
+        padding: 0,
+        minHeight: '100vh'
+      }}
+    >
       <div className="Tittle">
-        <div className="imagen-overlay"></div>
+        <div
+          style={{
+            position: 'absolute',
+            inset: 0,
+            backgroundImage: `linear-gradient(to top,rgb(26, 9, 9) 0%, rgba(3, 26, 53, 0.233), rgba(0, 0, 0, 0.2), transparent), url(${campNou})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center center',
+            opacity: 0.4,
+            zIndex: 1,
+            borderRadius: '10px'
+          }}
+        />
         <h1>FC BARCELONA - PRIMER EQUIPO</h1>
         <b>La plantilla de la temporada 2025/2026 todavía no es definitiva</b>
       </div>
@@ -68,17 +93,12 @@ function App() {
                       <b className="titulo-dato">Nacionalidad</b>
                       <span className="valor-dato">{player.nationality}</span>
                     </div>
-                    {/* <div className="dato-jugador">
-                      <b className="titulo-dato">Nacimiento</b>
-                      <span className="valor-dato">{player.birthdate}</span>
-                    </div> */}
                   </div>
                 </div>
               ))}
           </React.Fragment>
         ))}
 
-        {/* 🔥 Sección Director Técnico */}
         <h2 id="director tecnico" className="titulo-position">DIRECTOR TÉCNICO</h2>
         {players
           .filter(player => player.position_2 === 'DIRECTOR TECNICO')
